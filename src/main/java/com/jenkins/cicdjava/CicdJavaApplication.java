e9fbb017a3ca8d856dfc40c4fd176e4e16b7cc43
Mihran Marukyan <mihran.marukyan@ukg.com>
2023-03-03 15:21:26 +0400

    first

---
+++ src/main/java/com/jenkins/cicdjava/CicdJavaApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.cicdjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdJavaApplication.class, args);
+	}
+
+}
