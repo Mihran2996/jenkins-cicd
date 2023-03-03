815c337706a7d4d7111dce3d221bb92fad0470e9
Mihran Marukyan <mihran.marukyan@ukg.com>
2023-03-03 17:12:00 +0400

    added sout3

--- src/main/java/com/jenkins/cicdjava/CicdJavaApplication.java
+++ src/main/java/com/jenkins/cicdjava/CicdJavaApplication.java
@@ -10,6 +10,7 @@ public class CicdJavaApplication {
 		SpringApplication.run(CicdJavaApplication.class, args);
 		System.out.println("");
 		System.out.println("");
+		System.out.println("ddd");
 	}
 
 }
